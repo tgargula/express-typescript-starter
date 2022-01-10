8106c1580ec4318d0223ed9a5651201c89f4048e
tgargula <tomaszgargula1@gmail.com>
2022-01-10 19:55:05 +0100

    Improve integration between eslint and prettier; add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,4 @@
-import express from 'express';
+import './configure';
+import createServer from './server';
 
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(express.json());
-app.use(express.urlencoded());
-
-app.get('/', (req, res) => {
-  res.send('Hello there!');
-});
-
-app.listen(port, () => {
-  console.info(`The app is listening at http://localhost:${port}.`);
-});
+createServer();
---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const createServer = () => {
+  const app = express();
+  const port = process.env.PORT || 3000;
+
+  app.use(express.json());
+  app.use(express.urlencoded());
+
+  app.get('/', (req, res) => {
+    res.send('Hello there!');
+  });
+
+  app.listen(port, () => {
+    console.info(`The app is listening at http://localhost:${port}.`);
+  });
+};
+
+export default createServer;
